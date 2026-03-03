42b17399b9f486395d99b3b9fb45892ca2a02f46
Marcelo Augusto <123131698+marcelitos1v9@users.noreply.github.com>
2026-03-02 22:07:44 -0300

    refactor: remove obsolete patch scripts and mock usages from frontend and backend

--- back-end/patch_auth.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-c = open('c:/Users/fatec-dsm6/Desktop/adm_calm_wave/back-end/app/routes/auth.py', 'rb').read()
-c = c.replace(
-    b'data = request.get_json()\r\n    required = ["name", "email", "password"]\r\n    if not data or not all(k in data for k in required):\r\n        return jsonify({"error": "Nome, email e senha s\xc3\xa3o obrigat\xc3\xb3rios"}), 400',
-    b'try:\r\n        data = RegisterSchema().load(request.get_json() or {})\r\n    except ValidationError as err:\r\n        return jsonify({"error": "Parametros invalidos", "messages": err.messages}), 400'
-)
-open('c:/Users/fatec-dsm6/Desktop/adm_calm_wave/back-end/app/routes/auth.py', 'wb').write(c)
-print('OK')
